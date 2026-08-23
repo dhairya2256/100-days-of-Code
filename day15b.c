4515bef8827100170133ed59118b12d45bbaa077
dhairya2256 <67887532+dhairya2256@users.noreply.github.com>
2026-08-24 00:07:03 +0530

    Add files via upload
    Q29 (Loops without Arrays/Strings)
    Write a program to calculate the factorial of a number.
    
    Q30 (Loops without Arrays/Strings)
    Write a program to reverse a given number.

---
+++ day15b.c
@@ -0,0 +1,28 @@
+
+/*
+Q29 (Loops without Arrays/Strings)
+
+Write a program to calculate the factorial of a number.
+*/
+
+#include <stdio.h>
+
+int main() {
+	int number;
+	unsigned long long factorial = 1;
+
+	printf("Enter a number: ");
+	scanf("%d", &number);
+
+	if (number < 0) {
+		printf("Factorial is not defined for negative numbers.\n");
+	} else {
+		for (int i = 1; i <= number; i++) {
+			factorial *= i;
+		}
+
+		printf("Factorial of %d = %llu\n", number, factorial);
+	}
+
+	return 0;
+}
